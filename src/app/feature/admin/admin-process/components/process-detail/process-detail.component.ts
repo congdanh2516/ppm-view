06b533d8f0351aad27a5dc6675b8b0b0db113bed
lenhuthao1908 <nhuthaole1908@gmail.com>
2023-12-03 02:39:28 +0700

    fix process detail

--- src/app/feature/admin/admin-process/components/process-detail/process-detail.component.ts
+++ src/app/feature/admin/admin-process/components/process-detail/process-detail.component.ts
@@ -1,22 +1,16 @@
-import {
-  ChangeDetectorRef,
-  Component,
-  Input,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { MatMenuTrigger } from '@angular/material/menu';
 import { Task } from 'src/app/core/models/task';
 import { TaskService } from 'src/app/core/services/task/task.service';
 import { Project } from 'src/app/core/models/project';
 import { ProjectService } from 'src/app/core/services/project/project.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { NotificationBoxDeleteComponent } from './notificationBox/notification-box-delete/notification-box-delete.component';
 import { NotificationBoxUpdateComponent } from './notificationBox/notification-box-update/notification-box-update.component';
 import { TaskCreationComponent } from '../task-creation/task-creation.component';
 import { NotificationBoxUpdateDateComponent } from './notificationBox/notification-box-update-date/notification-box-update-date.component';
-import { format, time } from 'src/app/utils/date-utils';
+import { format } from 'src/app/utils/date-utils';
 import { Subtask } from 'src/app/core/models/subtask';
 import { TaskModificationComponent } from '../task-modification/task-modification.component';
 import { NotificationBoxCreateSubtaskComponent } from './notificationBox/notification-box-create-subtask/notification-box-create-subtask.component';
@@ -28,10 +22,36 @@ import { AdminProcessService } from '../../services/admin-process/admin-process.
   templateUrl: './process-detail.component.html',
   styleUrls: ['./process-detail.component.scss'],
 })
-export class ProcessDetailComponent implements OnInit {
+export class ProcessDetailComponent {
+  @ViewChild(MatMenuTrigger) trigger!: MatMenuTrigger;
+  @Input() task: Task;
+  @Input() tasklist: Task[] = [];
+  @Input() subtasklist: Subtask[] = [];
+  @Input() subtask: Subtask;
+  @Input() projectList: Project[] = [];
   isDisabledName: boolean = true;
   isDisabledInfor: boolean = true;
   isSubtasks: Array<string> = [];
+  project: any;
+  taskList: Array<any> = [];
+  projectId: string = '';
+  panelOpenState = false;
+  isLoading: boolean = false;
+
+  constructor(
+    private taskService: TaskService,
+    private projectService: ProjectService,
+    private route: ActivatedRoute,
+    public dialog: MatDialog,
+    private subtaskService: SubtaskService,
+    private adminProcessSV: AdminProcessService
+  ) {
+    this.route.params.subscribe((params: any) => {
+      this.projectId = params.id;
+      this.getProjectById(params.id);
+    });
+    this.getTaskList();
+  }
 
   removeDisabledName() {
     if (this.isDisabledName) {
@@ -49,47 +69,10 @@ export class ProcessDetailComponent implements OnInit {
     }
   }
 
-  @ViewChild(MatMenuTrigger)
-  trigger!: MatMenuTrigger;
-
   someMethod() {
     this.trigger.openMenu();
   }
 
-  project: any;
-  @Input() task: Task;
-  @Input() tasklist: Task[] = [];
-  @Input() subtasklist: Subtask[] = [];
-  @Input() subtask: Subtask;
-  @Input() projectList: Project[] = [];
-
-  taskList: Array<any> = [];
-  projectId: string = '';
-
-  panelOpenState = false;
-  isLoading: boolean = false;
-
-  constructor(
-    private taskService: TaskService,
-    private projectService: ProjectService,
-    private route: ActivatedRoute,
-    public dialog: MatDialog,
-    private cdr: ChangeDetectorRef,
-    private subtaskService: SubtaskService,
-    private adminProcessSV: AdminProcessService
-  ) {
-    this.route.params.subscribe((params: any) => {
-      this.projectId = params.id;
-      this.getProjectById(params.id);
-    });
-    this.getTaskList();
-  }
-
-  handleOnClick(event: Event): void {
-    event.stopPropagation();
-  }
-
-  ngOnInit(): void {}
   show(x: any) {
     console.log('x: ', x);
   }
@@ -100,6 +83,42 @@ export class ProcessDetailComponent implements OnInit {
 
   trackByFnn(index: any, item: any) {
     return item.subTaskId;
+  }
+
+  handleOnClick(event: Event): void {
+    event.stopPropagation();
+  }
+
+  handleUpdateName(event: Event): void {
+    if (!this.isDisabledName) {
+      this.project.projectName = (event.target as HTMLInputElement).value;
+      this.isDisabledName = true;
+      this.updateDialogProjectName(this.project);
+    }
+  }
+
+  handleUpdateInfor(event: Event): void {
+    if (!this.isDisabledInfor) {
+      this.project.projectStartAt = (event.target as HTMLInputElement).value;
+      this.isDisabledInfor = true;
+      this.updateDialogProjectDate(this.project);
+    }
+  }
+
+  handleCreateSubtask(subtask: Subtask) {
+    this.createSubtask(subtask);
+  }
+
+  getProjectList() {
+    this.projectService.getProjectList().subscribe({
+      next: (project: any) => {
+        this.projectList = project;
+        console.log('call api get all projects successfully!');
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
   }
 
   getTaskList() {
@@ -134,15 +153,32 @@ export class ProcessDetailComponent implements OnInit {
     });
   }
 
-  getProjectList() {
-    this.projectService.getProjectList().subscribe({
-      next: (project: any) => {
-        this.projectList = project;
-        console.log('call api get all projects successfully!');
+  getSubtasks(taskId: any) {
+    if (this.isSubtasks.includes(taskId)) {
+      this.isSubtasks = this.isSubtasks.filter((item) => item !== taskId);
+      this.subtasklist = this.subtasklist.filter(
+        (item) => item.taskParentId !== taskId
+      );
+    } else {
+      this.taskService.getSubtaskList(taskId).subscribe({
+        next: (subTasks: any) => {
+          this.isSubtasks.push(taskId);
+          this.subtasklist = [...this.subtasklist, ...subTasks];
+        },
+        error: (error: any) => {
+          console.log(error);
+        },
+      });
+    }
+  }
+
+  getProjectListNSchedule() {
+    this.adminProcessSV.scheduleProcess(this.projectId).subscribe({
+      next: (res) => {
+        this.getTaskList();
+        this.getProjectById(this.projectId);
       },
-      error: (error) => {
-        console.log(error);
-      },
+      error: (error) => {},
     });
   }
 
@@ -165,52 +201,63 @@ export class ProcessDetailComponent implements OnInit {
     });
   }
 
-  deleteDialogTask(task: Task): void {
-    const dialogRef = this.dialog.open(NotificationBoxDeleteComponent, {
-      data: task,
+  createDialogSubtask(task: Task) {
+    const dialogRef = this.dialog.open(NotificationBoxCreateSubtaskComponent, {
+      width: '500px',
+      data: {
+        taskId: task.taskId,
+        taskName: task.taskName,
+      },
     });
-    dialogRef.afterClosed().subscribe((response) => {
-      //response: {action: true}
-      if (response != undefined) {
+
+    dialogRef.afterClosed().subscribe((data) => {
+      this.getTaskList();
+    });
+  }
+
+  createDialogTask(projectId: any) {
+    const dialogRef = this.dialog.open(TaskCreationComponent, {
+      disableClose: true,
+      width: '500px',
+      height: 'auto',
+      data: {
+        projectId: projectId,
+        task: this.task,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((data) => {
+      if (data !== undefined) {
         this.getProjectListNSchedule();
       }
     });
   }
 
-  deleteDialogSubTask(subTask: Subtask): void {
-    const dialogRef = this.dialog.open(NotificationBoxDeleteComponent, {
-      width: '500px',
-      data: {
-        taskId: subTask.subTaskId,
-        taskName: subTask.subTaskName,
+  createSubtask(subtask: Subtask) {
+    this.subtaskService.createSubtask(subtask).subscribe({
+      next: (subtask: any) => {
+        this.subtask = subtask;
+        this.route.params.subscribe((params: any) => {
+          this.getProjectById(params.id);
+          this.taskService.getTaskList();
+        });
       },
-    });
-    dialogRef.afterClosed().subscribe((response) => {
-      console.log('Dialog closed with result:', response.taskId);
-      this.subtaskService.deleteSubtask(response.taskId).subscribe(() => {
-        this.subtasklist = this.subtasklist.filter(
-          (item) => item.subTaskId !== response.taskId
-        );
-        console.log('taskList handle delete' + this.tasklist);
-      });
+      error: (error) => {
+        console.log(error);
+      },
     });
   }
 
   updateDialogProjectName(project: Project) {
     const dialogRef = this.dialog.open(NotificationBoxUpdateComponent, {
-      width: '500px',
       data: {
         projectName: project.projectName,
         projectId: project.projectId,
       },
     });
-    dialogRef.afterClosed().subscribe((response) => {
-      console.log('updated project successfully: ' + response);
-      this.projectService.updateProject(response).subscribe(() => {
-        console.log('handle update successfully:' + this.project);
-        this.route.params.subscribe((params: any) => {
-          this.getProjectById(params.id);
-        });
+    dialogRef.afterClosed().subscribe(() => {
+      this.route.params.subscribe((params: any) => {
+        this.getProjectById(params.id);
       });
     });
   }
@@ -251,95 +298,19 @@ export class ProcessDetailComponent implements OnInit {
 
   updateDialogProjectDate(project: Project) {
     const dialogRef = this.dialog.open(NotificationBoxUpdateDateComponent, {
-      width: '500px',
       data: {
         projectStartAt: project.projectStartAt,
         projectId: project.projectId,
       },
     });
-    dialogRef.afterClosed().subscribe((response) => {
-      console.log('updated project successfully: ' + response);
-      this.projectService.updateProject(response).subscribe(() => {
-        console.log('handle update successfully:' + this.project);
-        this.route.params.subscribe((params: any) => {
+    dialogRef.afterClosed().subscribe(() => {
+      this.route.params.subscribe((params: any) => {
+        this.adminProcessSV.scheduleProcess(params.id).subscribe((data) => {
+          console.log('modify start date: ', data);
+          this.getTaskList();
           this.getProjectById(params.id);
         });
-        this.adminProcessSV
-          .scheduleProcess(response.projectId)
-          .subscribe((data) => {
-            console.log('modify start date: ', data);
-            this.getTaskList();
-          });
       });
-    });
-  }
-
-  createDialogSubtask(task: Task) {
-    const dialogRef = this.dialog.open(NotificationBoxCreateSubtaskComponent, {
-      width: '500px',
-      data: {
-        taskId: task.taskId,
-        taskName: task.taskName,
-      },
-    });
-
-    dialogRef.afterClosed().subscribe((data) => {
-      this.getTaskList();
-    });
-  }
-
-  createDialogTask(projectId: any) {
-    const dialogRef = this.dialog.open(TaskCreationComponent, {
-      disableClose: true,
-      width: '500px',
-      height: 'auto',
-      data: {
-        projectId: projectId,
-        task: this.task,
-      },
-    });
-
-    dialogRef.afterClosed().subscribe((data) => {
-      //data: {action: true}
-      if (data !== undefined) {
-        // this.getTaskList();
-        this.getProjectListNSchedule();
-      }
-    });
-  }
-
-  handleUpdateName(event: Event): void {
-    if (!this.isDisabledName) {
-      this.project.projectName = (event.target as HTMLInputElement).value;
-      this.isDisabledName = true;
-      this.updateDialogProjectName(this.project);
-    }
-  }
-
-  handleUpdateInfor(event: Event): void {
-    if (!this.isDisabledInfor) {
-      this.project.projectStartAt = (event.target as HTMLInputElement).value;
-      this.isDisabledInfor = true;
-      this.updateDialogProjectDate(this.project);
-    }
-  }
-
-  handleCreateSubtask(subtask: Subtask) {
-    this.createSubtask(subtask);
-  }
-
-  createSubtask(subtask: Subtask) {
-    this.subtaskService.createSubtask(subtask).subscribe({
-      next: (subtask: any) => {
-        this.subtask = subtask;
-        this.route.params.subscribe((params: any) => {
-          this.getProjectById(params.id);
-          this.taskService.getTaskList();
-        });
-      },
-      error: (error) => {
-        console.log(error);
-      },
     });
   }
 
@@ -357,34 +328,25 @@ export class ProcessDetailComponent implements OnInit {
     });
   }
 
-  getSubtasks(taskId: any) {
-    console.log('taskId: ', taskId);
-
-    if (this.isSubtasks.includes(taskId)) {
-      this.isSubtasks = this.isSubtasks.filter((item) => item !== taskId);
-      this.subtasklist = this.subtasklist.filter(
-        (item) => item.taskParentId !== taskId
-      );
-    } else {
-      this.taskService.getSubtaskList(taskId).subscribe({
-        next: (subTasks: any) => {
-          this.isSubtasks.push(taskId);
-          this.subtasklist = [...this.subtasklist, ...subTasks];
-        },
-        error: (error: any) => {
-          console.log(error);
-        },
-      });
-    }
+  deleteDialogTask(task: Task): void {
+    const dialogRef = this.dialog.open(NotificationBoxDeleteComponent, {
+      data: task,
+    });
+    dialogRef.afterClosed().subscribe((response) => {
+      if (response != undefined) {
+        this.getTaskList();
+      }
+    });
   }
 
-  getProjectListNSchedule() {
-    this.adminProcessSV.scheduleProcess(this.projectId).subscribe({
-      next: (res) => {
+  deleteDialogSubTask(subTask: Subtask): void {
+    const dialogRef = this.dialog.open(NotificationBoxDeleteComponent, {
+      data: subTask,
+    });
+    dialogRef.afterClosed().subscribe((response) => {
+      if (response != undefined) {
         this.getTaskList();
-        this.getProjectById(this.projectId);
-      },
-      error: (error) => {},
+      }
     });
   }
 }
