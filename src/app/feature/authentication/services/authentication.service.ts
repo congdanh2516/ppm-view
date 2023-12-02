ec4c1d41e6004236de2a1f914ff4a963f2d26d1f
lenhuthao1908 <nhuthaole1908@gmail.com>
2023-12-03 05:13:52 +0700

    call api delete process

--- src/app/feature/authentication/services/authentication.service.ts
+++ src/app/feature/authentication/services/authentication.service.ts
@@ -1,4 +1,8 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, tap, throwError } from 'rxjs';
@@ -7,48 +11,45 @@ import { ToastBoxModalService } from 'src/app/core/services/toast-box-modal.serv
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
+  userId: string = '';
 
-  userId: string = ""
-
-  constructor(private httpClient: HttpClient, 
-              private router: Router, 
-              private toastSV: ToastBoxModalService,
-              private localStorageSV: LocalStorageService
-  ) { }
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router,
+    private toastSV: ToastBoxModalService,
+    private localStorageSV: LocalStorageService
+  ) {}
 
   signIn(account: any) {
     let api: string = environment.url + 'user/login';
     let accountCode = btoa(account.username + ':' + account.password);
-    let header = new HttpHeaders()
-      .set('Authorization', `Basic ${accountCode}`);
-    return this.httpClient.get(api, {headers: header})
-    .pipe(
+    let header = new HttpHeaders().set('Authorization', `Basic ${accountCode}`);
+    return this.httpClient.get(api, { headers: header }).pipe(
       tap((data: any) => {
         this.localStorageSV.setItem('name', data.username);
         this.userId = data.userId;
         this.router.navigateByUrl('/admin/process/list');
         this.toastSV.sendMessage({
           isDisplay: true,
-          message: "Đăng nhập thành công",
-          icon: 'success'
-        })
+          message: 'Đăng nhập thành công',
+          icon: 'success',
+        });
       }),
       catchError((): any => {
         this.toastSV.sendMessage({
           isDisplay: true,
-          message: "Lỗi. VUi lòng thử lại sau!",
-          icon: 'error'
-        })
+          message: 'Lỗi. VUi lòng thử lại sau!',
+          icon: 'error',
+        });
       })
-    )
-
+    );
   }
 
   logOut() {
-    this.router.navigateByUrl("/sign-in");
+    this.router.navigateByUrl('/sign-in');
   }
 
   private handleError(error: HttpErrorResponse) {
@@ -56,8 +57,12 @@ export class AuthenticationService {
       console.error('An error occurred:', error.error);
     } else {
       console.error(
-        `Backend returned code ${error.status}, body was: `, error.error);
+        `Backend returned code ${error.status}, body was: `,
+        error.error
+      );
     }
-    return throwError(() => new Error('Something bad happened; please try again later.'));
+    return throwError(
+      () => new Error('Something bad happened; please try again later.')
+    );
   }
 }
